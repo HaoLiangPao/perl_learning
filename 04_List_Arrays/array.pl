6d77df1523242a0e21a9b16d4187df71b1b35cc3
Hao Liang <Hao.Liang@ibm.com>
2023-05-12 12:00:58 -0400

    Starting learning subroutine

--- 04_List_Arrays/array.pl
+++ 04_List_Arrays/array.pl
@@ -1,9 +1,12 @@
+# Pushing values into an array
 @my_array = qw/ 1 2 3/;
 $my_string_array = join(",", @my_array);
-
 print("$my_string_array\n");
 
+
+# Foreach loop
 foreach $number (@my_array)
 {
     print("Number is: "."$number\n");
 }
+
